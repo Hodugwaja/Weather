b61adfc553824ca0bec31c6bf7101360834020f3
HoduGwaja <honeybutter9250@gmail.com>
2020-08-11 10:32:27 +0900

    [commit | 2020.08.11 | added askign Permissions]

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { Alert } from 'react-native';
 import Loading from "./loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component{
   getLocation = async() => { 
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      const response = await Location.requestPermissionsAsync();
+      console.log(response);
+      const location = await Location.getCurrentPositionAsync(); 
+      console.log(location);
+    }catch (error) {
+      Alert.alert("Cannot Find your Location :(", "So Sad");
+    }
   };
   componentDidMount() {
     this.getLocation();
