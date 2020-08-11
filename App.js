db9540810d6319b0bad31d626d2c61bcbfd0153f
HoduGwaja <honeybutter9250@gmail.com>
2020-08-11 11:43:14 +0900

    [commit | 2020.08.11 | added getting Weather]

--- App.js
+++ App.js
@@ -3,14 +3,27 @@ import React from 'react';
 import { Alert } from 'react-native';
 import Loading from "./loading";
 import * as Location from "expo-location";
+import axios from "axios";
 
+const API_KEY = "2ab4483cca36c8430712a93c72634c19";
 export default class extends React.Component{
+  state = {
+    isloading : true
+  };
+  getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+  };
   getLocation = async() => { 
     try {
-      const response = await Location.requestPermissionsAsync();
-      console.log(response);
-      const location = await Location.getCurrentPositionAsync(); 
-      console.log(location);
+      const {
+        coords : {latitude, longitude}
+      } = await Location.getCurrentPositionAsync(); 
+      this.getWeather(latitude, longitude);
+      this.setState({isLoading : false});
+      console.log(coords.latitude, coords.longitude);
     }catch (error) {
       Alert.alert("Cannot Find your Location :(", "So Sad");
     }
@@ -19,9 +32,10 @@ export default class extends React.Component{
     this.getLocation();
   }
   render(){
+    const { isLoading } = this.state;
     return(
-      <Loading></Loading>
+      isLoading ? <Loading /> : null
     )
   }
 }
-
+ 
