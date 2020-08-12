cfc1c207cfee57d71dc5c1e18d99b5a75a4ac736
HoduGwaja <honeybutter9250@gmail.com>
2020-08-12 10:56:10 +0900

    [commit | 2020.08.12 | added getting condition names]

--- App.js
+++ App.js
@@ -1,41 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Alert } from 'react-native';
-import Loading from "./loading";
+import React from "react";
+import { Alert } from "react-native";
+import Loading from "./Loading";
 import * as Location from "expo-location";
 import axios from "axios";
 import Weather from "./Weather";
 
-const API_KEY = "2ab4483cca36c8430712a93c72634c19";
-export default class extends React.Component{
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+
+export default class extends React.Component {
   state = {
-    isloading : true
+    isLoading: true
   };
-  getWeather = async(latitude, longitude) => {
-    const { data } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
-    this.setState({isLoading:false, temp : data.main.temp});
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp
+    });
   };
-  getLocation = async() => { 
+  getLocation = async () => {
     try {
+      await Location.requestPermissionsAsync();
       const {
-        coords : {latitude, longitude}
-      } = await Location.getCurrentPositionAsync(); 
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      console.log(coords.latitude, coords.longitude);
-    }catch (error) {
-      Alert.alert("Cannot Find your Location :(", "So Sad");
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
     }
-    this.getLocation();
   };
-  
-  
-  render(){
-    const { isLoading, temp } = this.state;
-    return(
-      isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
     );
   }
 }
- 
